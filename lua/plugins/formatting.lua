e9b38fdc7635f6f968cc8aab3af2d5d41617f157
Alexander <alex.levinkoopman85@gmail.com>
2024-07-25 16:08:36 -0700

    formatting and linting

---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,41 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				graphql = { "prettier" },
+				lua = { "stylua" },
+				python = { "ruff" },
+				cpp = { "clang_format" },
+				c = { "clang_format" },
+				cs = { "clang_format" },
+				java = { "clang_format" },
+				rust = { "rustfmt" },
+				sql = { "sqlfmt" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			},
+		})
+		vim.keymap.set({ "n", "v" }, "<leader>gf", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
+}
